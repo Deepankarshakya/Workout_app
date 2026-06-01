91c10c2bb3ed72de70125087e7eb79ee4ed7033a
unknown <shakyadeepankar002@gmail.com>
2026-06-01 15:14:07 +0545

    (fix): Added (import { GestureHandlerRootView } from 'react-native-gesture-handler';) in the app.tsx for solving NativeViewGestureHandler, (style): Changed the Planner screen UI form for mobile compability

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import Navigation from './navigation';
 import useCachedResources from './hooks/useCachedResources';
+// 1. Import the gesture handler wrapper at the very top
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
-
-
   const isLoading = useCachedResources();
- 
 
-  if(isLoading){
-   return (
-    <>
-      <Navigation />
-      <StatusBar style="auto" />
-    </>
-  );   
-  }else{
+  if (isLoading) {
+    return (
+      // 2. Wrap everything inside the conditional return block
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Navigation />
+        <StatusBar style="auto" />
+      </GestureHandlerRootView>
+    );   
+  } else {
     return null;
   }
 }
